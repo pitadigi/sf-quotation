4e2fbfeabac3920c4fa69b3f1c025de6a61b3b08
Koji Matae <kmatae@pitadigi.jp>
2019-12-04 08:01:10 +0900

    Created Readme

--- force-app/main/default/aura/pdQuotation/pdQuotationController.js
+++ force-app/main/default/aura/pdQuotation/pdQuotationController.js
@@ -1,7 +1,9 @@
 ({
     doPrint : function(component, event, helper) {
+        setTimeout(() => {
+            $A.get("e.force:closeQuickAction").fire();
+        }, 1000);
         let recordId = component.get("v.recordId");
         window.open('/apex/pdQuotation?id=' + recordId, '_blank');
-        $A.get("e.force:closeQuickAction").fire();
     }
 })
