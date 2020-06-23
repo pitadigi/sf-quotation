1c5f8298bf976d9623507e5a9c05a36c861bdc89
kmatae-pitadigi <kmatae@pitadigi.jp>
2020-06-24 07:06:07 +0900

    Changed quote title

--- force-app/main/default/aura/pdQuotation/pdQuotationController.js
+++ force-app/main/default/aura/pdQuotation/pdQuotationController.js
@@ -1,7 +1,11 @@
 ({
     doPrint : function(component, event, helper) {
         setTimeout(() => {
-            $A.get("e.force:closeQuickAction").fire();
+            var element = document.getElementsByClassName("DESKTOP uiModal forceModal");    
+            element.forEach(function(e, t) {
+                $A.util.addClass(e, 'slds-hide');
+            });     
+            $A.get("e.force:refreshView").fire();
         }, 1000);
         let recordId = component.get("v.recordId");
         window.open('/apex/pdQuotation?id=' + recordId, '_blank');
